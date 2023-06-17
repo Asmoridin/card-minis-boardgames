fcc5b1c80f9a25b02f4fcc4337b5eaa0eac59ea2
Steve P <steven.pow@gmail.com>
2023-06-17 12:08:35 -0400

    Version 1.0 of ChampionsWL is done!

--- win_loss/MarvelChampionsWL.py
+++ win_loss/MarvelChampionsWL.py
@@ -58,29 +58,54 @@ def getAspectStats(playMap):
         for aspect in aspect_tuple:
             sum_map[aspect] += playMap[(hero, aspect_tuple)][0]
     # TODO: Tuple the sum map, sort it
-    print("Aspect sorting to do")
-    print(sum_map)
-    return()
+    aspect_tuples = []
+    for aspect in sum_map:
+        aspect_tuples.append((aspect, sum_map[aspect]))
+    aspect_tuples = sorted(aspect_tuples, key=lambda x:(x[1], x[0]))
+    return(aspect_tuples[-1][0], aspect_tuples[-1][1], aspect_tuples[0][0], aspect_tuples[0][1])
 
 def getLeastPlayedHero(playMap):
     # Return the least played hero/aspect combination
     least_played_hero = getHeroStats(playMap)[2]
     combinations = ChampHeroes.hero_map[least_played_hero].genCombos()
-    print(combinations)
-    # TODO: Finish this.  Basically, find the least played hero-aspect comination
-    return("Rogue", "Justice")
+    aspect_combos = []
+    for combination in combinations:
+        if combination not in playMap:
+            aspect_combos.append((combination, 0))
+        else:
+            aspect_combos.append((combination, playMap[combination]))
+    aspect_combos = sorted(aspect_combos, key=lambda x:(x[1], x[0][1]))
+    return(aspect_combos[0][0][0], aspect_combos[0][0][1])
 
 def getLeastPlayedEncounter(enc_played_map):
-    # Return the encounter combination played the least
-    return(("Rhino", "Bomb Scare"))
+    # First, get least played villain
+    chosen_villain = getVillainStats(enc_played_map)[2]
+    if chosen_villain == 'The Hood':
+        hood_encounters = sorted(random.sample(ChampEncounters.modular_encounters, 7))
+        this_encounter = ('The Hood', tuple(hood_encounters))
+        return this_encounter
+    encounter_list = ChampEncounters.encounter_map[chosen_villain].genCombos()
+    encounter_list = sorted(encounter_list, key=lambda x:x[1])
+    encounter_played = []
+    for encounter_combo in encounter_list:
+        if encounter_combo not in enc_played_map:
+            return encounter_combo
+        encounter_played.append((encounter_combo, enc_played_map[encounter_combo][0]))
+    encounter_played = sorted(encounter_played, key=lambda x: x[1])
+    return(encounter_played[0][0])
 
 def getVillainStats(encounter_map):
     # Returns (most played villain, most played villain amount, least played villain, least played villain amount)
     sum_map = {}
     for villain in ChampEncounters.encounters:
         sum_map[villain.name] = 0
-    # TODO: Do the calculations, sort it, and figure it out
-    return("Klaw", 100, 'Rhino', 0)
+    for played_enc in encounter_map:
+        sum_map[played_enc[0]] += encounter_map[played_enc][0]
+    sum_tuples = []
+    for villain in sum_map:
+        sum_tuples.append((villain, sum_map[villain]))
+    sum_tuples = sorted(sum_tuples, key=lambda x:(x[1], x[0]))
+    return(sum_tuples[-1][0], sum_tuples[-1][1], sum_tuples[0][0], sum_tuples[0][1])
 
 def getModularStats(encounter_map):
     # Return most played encounter set, amount it was played, least played encounter set, amount played
@@ -88,8 +113,16 @@ def getModularStats(encounter_map):
     sum_map = {}
     for modular in modular_sets:
         sum_map[modular] = 0
-    #TODO: tuple it, sort it, return it
-    return("Ship Command", 100, "Inheritors", 0)
+    for played_enc in encounter_map:
+        for modular in ChampEncounters.get_req_by_encounter(played_enc[0]):
+            sum_map[modular] += encounter_map[played_enc][0]
+        for modular in played_enc[1]:
+            sum_map[modular] += encounter_map[played_enc][0]
+    mod_tuples = []
+    for modular in sum_map:
+        mod_tuples.append((modular, sum_map[modular]))
+    mod_tuples = sorted(mod_tuples, key=lambda x:(x[1], x[0]))
+    return(mod_tuples[-1][0], mod_tuples[-1][1], mod_tuples[0][0], mod_tuples[0][1])
 
 # Read in and set up Hero data structures
 if os.getcwd().endswith('card-minis-boardgames'):
@@ -110,6 +143,26 @@ for line in hero_lines:
         aspects = tuple(hero_line[1].split('/'))
     hero_played_map[(hero_line[0], aspects)] = (int(hero_line[2]), int(hero_line[3]))
 
+# Read in and process the Encounter data structures
+if os.getcwd().endswith('card-minis-boardgames'):
+    file_h = open('win_loss/DB/ChampionsPlayedEncounters.txt', 'r')
+else:
+    file_h = open('DB/ChampionsPlayedEncounters.txt', 'r')
+encounter_lines = file_h.readlines()
+file_h.close()
+encounter_lines = [line.strip() for line in encounter_lines]
+enc_played_map = {}
+played_max = 0
+for line in encounter_lines:
+    #TODO: encounter_line[2] is the difficulty, do we want to do anything with it?
+    encounter_line = line.split(';')
+    if int(encounter_line[3]) > played_max:
+        played_max = int(encounter_line[3])
+    encounter_sets = tuple(sorted(encounter_line[1].split('/')))
+    if encounter_sets == ('',):
+        encounter_sets = ()
+    enc_played_map[(encounter_line[0], encounter_sets)] = (int(encounter_line[3]), int(encounter_line[4]))
+
 if __name__ == "__main__":
     print("Generating a game")
     print("There are %d different Hero/Aspect combinations, and %d different game pairings" % (total_hero_choices, determineCombinations()))
@@ -119,6 +172,78 @@ if __name__ == "__main__":
 
     # Choose first hero - always choose least played hero
     choice_1 = getLeastPlayedHero(hero_played_map)
-
-    # Choose second hero, with lowest hero/aspect combination remaining
     
+    # Choose second hero, with lowest hero/aspect combination remaining
+    choices = []
+    for hero_choice in ChampHeroes.hero_combinations:
+        if hero_choice[0] == choice_1[0]:
+            continue
+        valid = True
+        for aspect in hero_choice[1]:
+            if aspect in choice_1[1]:
+                valid = False
+        if not valid:
+            continue
+        if (hero_choice[0], hero_choice[1]) in hero_played_map:
+            choices.append((hero_choice[0], hero_choice[1], hero_played_map[(hero_choice[0], hero_choice[1])][0]))
+        else:
+            choices.append((hero_choice[0], hero_choice[1], 0))
+    choices = sorted(choices, key=lambda x:(x[2], x[0], x[1]))
+    choice_2 = (choices[0][0], choices[0][1])
+
+    hero_tuple = getHeroStats(hero_played_map)
+    print("Most played hero: %s (%d times). Least: %s (%d)" % (hero_tuple))
+    aspect_tuple = getAspectStats(hero_played_map)
+    print("Most played aspect: %s (%d times). Least: %s (%d)" % (aspect_tuple))
+    scenario_tuple = getVillainStats(enc_played_map)
+    print("Most played scenario: %s (%d times). Least: %s (%d)" % (scenario_tuple))
+    modular_tuple = getModularStats(enc_played_map)
+    print("Most played modular encounter: %s (%d times). Least: %s (%d)" % (modular_tuple))
+
+    # Choose an encounter
+    encounter_choice = getLeastPlayedEncounter(enc_played_map)
+
+    hero_1_wl = getHeroWL(choice_1[0], choice_1[1], hero_played_map)
+    hero_2_wl = getHeroWL(choice_2[0], choice_2[1], hero_played_map)
+    print("\nHeroes/Aspects chosen: %s %s (%sW-%sL) and %s %s (%sW-%sL)" % ('/'.join(choice_1[1]), choice_1[0], hero_1_wl[0], hero_1_wl[1], '/'.join(choice_2[1]), choice_2[0], hero_2_wl[0], hero_2_wl[1]))
+    enc_wl = getEncounterWL(encounter_choice[0], encounter_choice[1], enc_played_map)
+    print("Chosen encounter is %s %s (%sW-%sL)" % ('/'.join(encounter_choice[1]), encounter_choice[0], enc_wl[0], enc_wl[1]))
+
+    print("\nTotal W-L by hero:")
+    hero_wl = {}
+    for hero_choice in ChampHeroes.hero_combinations:
+        if hero_choice[0] not in hero_wl:
+            hero_wl[hero_choice[0]] = [0,0]
+        wins, losses = getHeroWL(hero_choice[0], hero_choice[1], hero_played_map)
+        hero_wl[hero_choice[0]][0] += wins
+        hero_wl[hero_choice[0]][1] += losses
+    for hero in sorted(hero_wl):
+        print("%s%s%d - %d" % (hero, " " * (30 - len(hero)), hero_wl[hero][0], hero_wl[hero][1]))
+
+    overall_wl = [0, 0]
+    print("\nTotal W-L by villain:")
+    villain_wl = {}
+    for villain_choice in enc_played_map:
+        if villain_choice[0] not in villain_wl:
+            villain_wl[villain_choice[0]] = [0,0]
+        wins, losses = getEncounterWL(villain_choice[0], villain_choice[1], enc_played_map)
+        villain_wl[villain_choice[0]][0] += wins
+        overall_wl[0] += wins
+        villain_wl[villain_choice[0]][1] += losses
+        overall_wl[1] += losses
+
+    for villain in sorted(villain_wl):
+        print("%s%s%d - %d" % (villain, " " * (30-len(villain)), villain_wl[villain][0], villain_wl[villain][1]))
+
+    print("\nTotal W-L by aspect:")
+    aspect_wl = {}
+    for hero_choice in ChampHeroes.hero_combinations:
+        for aspect in hero_choice[1]:
+            if aspect not in aspect_wl:
+                aspect_wl[aspect] = [0,0]
+            wins, losses = getHeroWL(hero_choice[0], hero_choice[1], hero_played_map)
+            aspect_wl[aspect][0] += wins
+            aspect_wl[aspect][1] += losses
+    print(aspect_wl)
+
+    print("\nWin Loss Records, Overall: %d - %d, %.3f" % (overall_wl[0], overall_wl[1], overall_wl[0]/(overall_wl[0] + overall_wl[1])))
