798b2ace1785a201c5c66dc45f4fec5ca90f6781
Steve P <steven.pow@gmail.com>
2023-06-24 15:51:46 -0400

    Update DragonDice.py

--- card_games/DragonDice.py
+++ card_games/DragonDice.py
@@ -4,8 +4,11 @@ import sys, os
 
 GAME_NAME = "Dragon Dice"
 
+army_factions = ['Dwarves', 'Coral Elves', 'Goblins', 'Lava Elves', 'Amazons', 'Fire Walkers', 'Undead', 'Feral', 'Swamp Stalkers', 'Frostwings', 'Scalders', 'Treefolk', 'Eldarim', 'Dracolem']
+valid_factions = army_factions + ['Medallion', 'Items', 'Dragons', 'Dragonkin', 'Relics', 'Major Terrain', 'Minor Terrain', 'Special Terrain']
+
 def getMetaType(in_type):
-    if in_type in ['Dwarves', 'Coral Elves', 'Goblins', 'Lava Elves', 'Amazons', 'Fire Walkers', 'Undead', 'Feral', 'Swamp Stalkers', 'Frostwings', 'Scalders', 'Treefolk', 'Eldarim', 'Dracolem']:
+    if in_type in army_factions:
         return 'Unit'
     elif in_type in ['Special Terrain', 'Major Terrain', 'Minor Terrain']:
         return 'Terrain'
@@ -16,9 +19,6 @@ def getMetaType(in_type):
     else:
         print("Unhandled meta type: " + in_type)
         return('Unhandled')
-    
-valid_factions = ['Dwarves', 'Coral Elves', 'Goblins', 'Lava Elves', 'Amazons', 'Fire Walkers', 'Undead', 'Feral', 'Swamp Stalkers', 'Frostwings', 'Scalders', 'Treefolk', 'Eldarim', 
-    'Dracolem', 'Medallion', 'Items', 'Dragons', 'Dragonkin', 'Relics', 'Major Terrain', 'Minor Terrain', 'Special Terrain']
 
 if os.getcwd().endswith('card-minis-boardgames'):
     file_h = open('card_games/DB/DragonDiceCollection.txt', 'r')
@@ -46,3 +46,33 @@ for line in lines:
         else:
             dice_max = max(own, 2)
         faction_total[dice_faction] += (own * 3)
+    elif dice_size == "Uncommon":
+        if dice_faction in ['Items', 'Dragonkin']:
+            dice_max = max(own, 2)
+        else:
+            dice_max = max(own, 4)
+        faction_total[dice_faction] += (own * 2)
+    elif dice_size == "Common":
+        if dice_faction in ['Items', 'Dragonkin']:
+            dice_max = max(own, 4)
+        else:
+            dice_max = max(own, 8)
+        faction_total[dice_faction] += (own)
+    elif dice_size == "Monster" or dice_size == "Champion" or dice_size == "Artifact" or dice_size == "Dragon":
+        dice_max = max(own, 1)
+        faction_total[dice_faction] += (own * 4)
+    elif dice_faction == "Major Terrain":
+        dice_max = max(own, 2)
+    elif dice_faction in ['Minor Terrain', 'Special Terrain']:
+        dice_max = max(own, 1)
+    else:
+        print("Unhandled dice size: " + dice_size)
+    total_max += dice_max
+    total_own += own
+    item_list.append((dice_name, dice_faction, dice_size, own, dice_max))
+
+if len(sys.argv) > 1 and sys.argv[1] == '-s':
+    print("Summary:")
+    for faction in sorted(army_factions):
+        print("%s: %d" % (faction, faction_total[faction]))
+    os._exit(0)
