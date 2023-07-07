209159e35bd37400ee3b4e5b24b9740575457f75
Steve P <133926116+Asmoridin@users.noreply.github.com>
2023-07-07 11:19:29 -0400

    Create SFBRaces.py

---
+++ board_games/SFBRaces.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+alpha_races = ['Federation', 'Klingon', 'Kzinti', 'Hydran', 'WYN', 'Romulan', 'Andromedan', 'Orion', 'Gorn', 'Tholian', 'ISC', 'Lyran', 'LDR', 'Vudar',
+    'Jindarian', 'Nicozian', 'Borak', 'Carnivon', 'Paravian', 'Peladine', 'Seltorian']
+simulator_races = ['Barbarian', 'Britanian', "Cana'dien", 'Deltan', 'Flivver', 'Frax', 'Hispanolian', 'Qari', 'Sharkhunter', 'Triaxian', '8th Air Force']
+omega_races = ['Alunda', 'FRA', 'Bolosco', 'Branthodon', 'Chlorophon', 'Drex', 'Hiver', 'Iridani', 'Koligahr', 'Loriyill', 'Maesron', 'Probr',
+    'Qixa', 'Ryn', 'Sigvirion', 'Singer', 'Souldra', 'Trobrin', 'Vari', 'Worb', 'Ymatrian', 'Zosman']
+lmc_races = ['Baduvai', 'Eneen', 'Jumokian', 'Maghadim', 'Uthiki']
+triangulum_races = ['Helgardian', 'Arachnid', 'Mallaran', 'Imperium']
+
+all_race_names = alpha_races + simulator_races + omega_races + lmc_races + triangulum_races
+
+race_map = {'Alpha':alpha_races, 'Omega;:omega_races, 'Simulator':simulator_races, 'LMC':lmc_races, 'Triangulum':triangulum_races}
