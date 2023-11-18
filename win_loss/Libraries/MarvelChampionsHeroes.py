d20b7f6da37ac389b79b09bd036e0e3ae64eeeff
Steve P <steven.pow@gmail.com>
2023-11-17 19:24:32 -0500

    New Marvel Champions stuff is out!

--- win_loss/Libraries/MarvelChampionsHeroes.py
+++ win_loss/Libraries/MarvelChampionsHeroes.py
@@ -2,7 +2,7 @@
 
 import itertools
 
-aspects = ['Leadership', 'Justice', 'Protection', 'Aggression', ] # 'Pool
+aspects = ['Leadership', 'Justice', 'Protection', 'Aggression', "'Pool"]
 
 class Hero:
     def __init__(self, name, num_aspects=1):
@@ -26,7 +26,7 @@ heroes.extend([
     Hero('Spectrum'), Hero('Adam Warlock', 4), Hero('Nebula'), Hero('War Machine'), Hero('Vision'), Hero('Valkyrie'), 
     Hero('Spider-Man (Miles Morales)'), Hero('Ghost-Spider'), Hero('Nova'), Hero('Ironheart'), Hero('Spider-Ham'), Hero('Sp//Dr'),
     Hero('Colossus'), Hero('Shadowcat'), Hero('Cyclops'), Hero('Phoenix'), Hero('Wolverine'), Hero('Storm'), Hero('Gambit'), Hero('Rogue'),
-    Hero('Cable'), Hero('Domino'), Hero('Psylocke'), Hero('Angel'), #Hero('X-23'), Hero('Deadpool'),
+    Hero('Cable'), Hero('Domino'), Hero('Psylocke'), Hero('Angel'), Hero('X-23'), Hero('Deadpool'),
     #Hero('Bishop'), Hero('Magik'),
 ])
 
