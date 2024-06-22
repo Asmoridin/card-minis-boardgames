2a59a1db8f6f97c43f96457e4f1525d331bc84b7
Steve P <steven.pow@gmail.com>
2024-06-22 18:54:22 -0400

    Adding SWU games

--- win_loss/sw_unlimited_wl.py
+++ win_loss/sw_unlimited_wl.py
@@ -34,6 +34,8 @@ source_data_lines = [line.strip() for line in source_data_lines]
 valid_leaders = {}
 leader_games_map = {} # Total number of games I've seen the leader
 for line in source_data_lines:
+    if line.startswith('#') or line.strip() == '':
+        continue
     if line.split(';')[2] == 'Leader':
         leader_name = line.split(';')[0]
         aspects = []
