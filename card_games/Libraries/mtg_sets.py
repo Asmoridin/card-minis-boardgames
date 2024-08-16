4066baef32bc0777810d74623e5664efa10b7e34
Steve P <steven.pow@gmail.com>
2024-08-16 16:54:57 -0400

    More MTG work

--- card_games/Libraries/mtg_sets.py
+++ card_games/Libraries/mtg_sets.py
@@ -5,7 +5,65 @@ Library to more easily handle MTG sets
 """
 
 LEGACY_SETS = ["Limited Edition Alpha", "Limited Edition Beta", "Unlimited Edition", "Jumpstart",
-    "Revised Edition", "Fourth Edition", "Fifth Edition", "Classic Sixth Edition",
+    "Revised Edition", "Fourth Edition", "Fifth Edition", "Classic Sixth Edition", "Battlebond",
     "Double Masters 2022", "Ultimate Box Toppers", "Ultimate Masters", "Seventh Edition",
-    "Masterpiece Series: Kaladesh Inventions"]
-MTGO_SETS = ["Vintage Masters", "Masters Edition IV"]
+    "Masterpiece Series: Kaladesh Inventions", "Dominaria Remastered", "Jumpstart 2022", "Ice Age",
+    "Eternal Masters", "Archenemy", "Commander 2016", "Commander 2015", "Commander 2013 Edition",
+    "Commander Anthology 2018", "Duel Decks: Elspeth vs. Tezzeret", "Commander Anthology",
+    "Duel Decks: Ajani vs. Nicol Bolas", "Magic: The Gathering-Commander", "Beatdown Box Set",
+    "The Brothers' War Retro Artifacts", "From the Vault: Relics", "Bloomburrow Commander",
+    "The Lord of the Rings: Tales of Middle Earth Commander", "Modern Horizons 3 Commander",
+    "Outlaws of Thunder Junction Commander", "Murders at Karlov Manor Commander", "Commander 2021",
+    "Magic The Gathering—Fallout", "The Lost Caverns of Ixalan Commander", "Doctor Who Commander",
+    "Wilds of Eldraine Commander", "Commander Masters", "March of the Machine Commander", 
+    "Phyrexia: All Will Be One Commander", "Dominaria United Commander", "Commander 2018",
+    "Kamigawa: Neon Dynasty Commander", "The Brothers' War Commander", "Commander 2019",
+    "Warhammer 40,000 Commander", "Commander Legends: Battle for Baldur's Gate",
+    "Streets of New Capenna Commander", "Commander Collection: Green", "Kaldheim Commander",
+    "Adventures in the Forgotten Realms Commander", "Innistrad: Crimson Vow Commander", 
+    "Innistrad: Midnight Hunt Commander", "Commander Legends", "Ikoria Commander", "Mirage",
+    "Zendikar Rising Commander", "Commander 2017", "Commander 2014", "Starter 2000",
+    "Secret Lair Drop", "Duel Decks Anthology, Garruk vs. Liliana", "Portal", "Masters 25",
+    "Duel Decks: Garruk vs. Liliana", "Starter 1999", "Strixhaven Mystical Archive",
+    "Masterpiece Series: Amonkhet Invocations", "Duel Decks Anthology, Divine vs. Demonic",
+    "Duel Decks: Divine vs. Demonic", "From the Vault: Twenty", "Planechase", "Mercadian Masques",
+    "Duel Decks: Phyrexia vs. the Coalition", "Battle Royale Box Set", "Urza's Saga", "Tempest",
+    "Premium Deck Series: Graveborn", "Iconic Masters",
+    ]
+
+ARENA_SETS = ["Jumpstart: Historic Horizons", "Historic Anthology 1", ]
+
+# Sets that were only released on MTG Online.  We care about the rarity, but that's it.
+MTGO_SETS = ["Vintage Masters", "Masters Edition IV", "Masters Edition III", "Masters Edition",
+    "Double Masters", "Tempest Remastered", ]
+
+# Standard legal sets are legal in all other formats, mostly.
+STANDARD_SETS = ["Dominaria United", "The Brothers' War", "Phyrexia: All Will Be One",
+    "March of the Machine", "March of the Machine: The Aftermath", "Wilds of Eldraine",
+    "The Lost Caverns of Ixalan", "Murders at Karlov Manor", "Outlaws of Thunder Junction",
+    "The Big Score", "Bloomburrow"]
+
+# These sets are legal in Pioneer, Modern, and older formats
+PIONEER_SETS = ["Return to Ravnica", "Gatecrash", "Dragon's Maze", "Magic 2014 Core Set", "Theros",
+    "Born of the Gods", "Journey into Nyx", "Magic 2015 Core Set", "Khans of Tarkir",
+    "Fate Reforged", "Dragons of Tarkir", "Magic Origins", "Battle for Zendikar",
+    "Oath of the Gatewatch", "Welcome Deck 2016", "Shadows over Innistrad", "Eldritch Moon",
+    "Kaladesh", "Aether Revolt", "Welcome Deck 2017", "Amonkhet", "Hour of Devastation", "Ixalan",
+    "Rivals of Ixalan", "Dominaria", "Core Set 2019", "Guilds of Ravnica", "Ravnica Allegiance",
+    "War of the Spark", "Core Set 2020", "Throne of Eldraine", "Theros Beyond Death",
+    "Ikoria: Lair of Behemoths", "Core Set 2021", "Zendikar Rising", "Kaldheim",
+    "Strixhaven: School of Mages", "Adventures in the Forgotten Realms",
+    "Innistrad: Midnight Hunt", "Innistrad: Crimson Vow", "Kamigawa: Neon Dynasty",
+    "Streets of New Capenna"]
+
+# These are legal in Modern and older formats
+MODERN_SETS  = ["Eighth Edition", "Mirrodin", "Darksteel", "Fifth Dawn", "Champions of Kamigawa",
+    "Betrayers of Kamigawa", "Saviors of Kamigawa", "Ninth Edition", "Ravnica: City of Guilds",
+    "Guildpact", "Dissension", "Coldsnap", "Time Spiral", "Planar Chaos", "Future Sight",
+    "Tenth Edition", "Lorwyn", "Morningtide", "Shadowmoor", "Eventide", "Shards of Alara",
+    "Conflux", "Alara Reborn", "Magic 2010", "Zendikar", "Worldwake", "Rise of the Eldrazi",
+    "Magic 2011", "Scars of Mirrodin", "Mirrodin Besieged", "New Phyrexia", "Magic 2012",
+    "Innistrad", "Dark Ascension", "Avacyn Restored", "Magic 2013", "Modern Horizons 2",
+    "Modern Horizons", "The Lord of the Rings: Tales of Middle-Earth", "Modern Horizons 3",
+    "Assassin's Creed", 'Time Spiral "Timeshifted"',
+    ]
--- card_games/magic_gathering.py
+++ card_games/magic_gathering.py
@@ -8,22 +8,15 @@ import os
 import re
 import sys
 
+sys.path.append('.')
+from utils.output_utils import double_print
+from utils.sort_and_filter import sort_and_filter
+from card_games.Libraries import mtg_sets
+
 GAME_NAME = "Magic: The Gathering"
 
-if os.getcwd().endswith('card-minis-boardgames'):
-    file_h = open('card_games/DB/MTGCardData.txt', 'r', encoding="UTF-8")
-    restr_file_h = open('card_games/DB/MTGRestrictions.txt', 'r', encoding="UTF-8")
-    sys.path.append('.')
-    from utils.output_utils import double_print
-    from utils.sort_and_filter import sort_and_filter
-    from card_games.Libraries import mtg_sets
-else:
-    file_h = open('DB/MTGCardData.txt', 'r', encoding="UTF-8")
-    restr_file_h = open('DB/MTGRestrictions.txt', 'r', encoding="UTF-8")
-    sys.path.append('.')
-    from utils.output_utils import double_print
-    from utils.sort_and_filter import sort_and_filter
-    from Libraries import mtg_sets
+file_h = open('card_games/DB/MTGCardData.txt', 'r', encoding="UTF-8")
+restr_file_h = open('card_games/DB/MTGRestrictions.txt', 'r', encoding="UTF-8")
 
 def parse_restrictions(restr_lines):
     """
@@ -35,14 +28,14 @@ def parse_restrictions(restr_lines):
         return_dict[this_card_name] = {}
         for restriction in card_restrictions.split('/'):
             this_format, bnr = restriction.split('-')
-            if this_format not in ['Legacy', 'Vintage']:
+            if this_format not in ['Legacy', 'Vintage', 'Commander']:
                 print("Unknown format: " + this_format)
-            if bnr not in ['Banned', 'Restricted', 'Commander']:
+            if bnr not in ['Banned', 'Restricted']:
                 print("Unknown status: " + bnr)
             return_dict[this_card_name][this_format] = bnr
     return return_dict
 
-def parse_sets(card_name, card_set_string, card_restrictions):
+def parse_sets(this_card_name, card_set_string, card_restrictions):
     """
     The heavy lifting function of this module, where I go through and figure out rarites, sets,
     formats, and anything else
@@ -58,13 +51,32 @@ def parse_sets(card_name, card_set_string, card_restrictions):
             if this_set in mtg_sets.LEGACY_SETS:
                 ret_sets.append(this_set)
                 ret_rarities.add(this_set_rarity)
+            elif this_set in mtg_sets.ARENA_SETS:
+                pass # We don't care one bit about Arena sets
             elif this_set in mtg_sets.MTGO_SETS:
                 # We don't track the set, but the rarity matters
                 ret_rarities.add(this_set_rarity)
+            elif this_set in mtg_sets.STANDARD_SETS:
+                ret_sets.append(this_set)
+                ret_rarities.add(this_set_rarity)
+                ret_formats['Standard'] = 4
+                ret_formats['Pioneer'] = 4
+                ret_formats['Modern'] = 4
+            elif this_set in mtg_sets.PIONEER_SETS:
+                ret_sets.append(this_set)
+                ret_rarities.add(this_set_rarity)
+                ret_formats['Pioneer'] = 4
+                ret_formats['Modern'] = 4
+            elif this_set in mtg_sets.MODERN_SETS:
+                ret_sets.append(this_set)
+                ret_rarities.add(this_set_rarity)
+                ret_formats['Modern'] = 4
             else:
-                print("[" + card_name + "] Handle: " + this_set)
+                print("[" + this_card_name + "] Handle: " + this_set)
         else:
-            print("[" + card_name + "] Issue with: " + card_set)
+            print("[" + this_card_name + "] Issue with: " + card_set)
+    if 'Common' in ret_rarities or 'Land' in ret_rarities:
+        ret_formats['Pauper'] = 4
     if card_restrictions:
         for restriction_format, restriction_bnr in card_restrictions.items():
             if restriction_bnr == 'Banned':
@@ -74,6 +86,11 @@ def parse_sets(card_name, card_set_string, card_restrictions):
             else:
                 print(restriction_format)
                 print(restriction_bnr)
+    if len(ret_formats) == 1:
+        ret_formats = {"Vintage": 1}
+    for _, format_qty in ret_formats.items():
+        this_card_max = max(this_card_max, format_qty)
+    ret_rarities = list(ret_rarities)
     return(ret_sets, ret_rarities, ret_formats, this_card_max)
 
 def validate_colors(in_colors):
@@ -81,7 +98,7 @@ def validate_colors(in_colors):
     Takes in a string of colors, and returns a list with the full color names
     """
     ret_colors = []
-    color_map = {'C':'Colorless'}
+    color_map = {'C':'Colorless', 'B':'Black'}
     for color in in_colors.split('/'):
         if color not in color_map:
             print("Invalid color: " + color)
@@ -102,8 +119,13 @@ def validate_types(card_type_string):
     else:
         types = card_type_string
         subtypes = ''
+    if types == 'Basic Land':
+        ret_type.append(types)
+        types = ''
     for check_type in types.split(' '):
-        if check_type in ['Artifact', 'Creature']:
+        if check_type == '':
+            continue
+        if check_type in ['Artifact', 'Creature', 'Enchantment', 'Sorcery', "Instant"]:
             ret_type.append(check_type)
         else:
             print("Unknown type: " + check_type)
@@ -120,6 +142,7 @@ restr_file_h.close()
 
 TOTAL_OWN = 0
 TOTAL_MAX = 0
+raw_list = []
 for line in lines:
     if line == '' or line.startswith('#'):
         continue
@@ -127,4 +150,220 @@ for line in lines:
     card_qty = int(card_qty)
     card_colors = validate_colors(card_colors)
     card_type, card_subtype = validate_types(card_type)
-    card_sets, card_rarities, card_formats, card_max = parse_sets(card_name, card_sets, restrictions.get(card_name))
+    card_sets, card_rarities, card_formats, CARD_MAX = parse_sets(card_name, card_sets, \
+        restrictions.get(card_name))
+    if 'Basic Land' in card_type:
+        for card_format in card_formats:
+            card_formats[card_format] = 20
+            CARD_MAX = 20
+    TOTAL_MAX += CARD_MAX
+    TOTAL_OWN += card_qty
+    raw_list.append((card_name, card_type, card_subtype, card_colors, card_sets, card_rarities, \
+        card_formats, card_qty, CARD_MAX))
+
+# Get data for Vintage
+vintage_list = []
+VINTAGE_OWN = 0
+VINTAGE_TOTAL = 0
+for card in raw_list:
+    if 'Vintage' in card[6]:
+        vintage_list.append(card)
+        VINTAGE_TOTAL += card[6]['Vintage']
+        VINTAGE_OWN += min(card[7], card[6]['Vintage'])
+VINTAGE_CARDS = len(vintage_list)
+
+vintage_set, filtered_list = sort_and_filter(vintage_list, 4)
+vintage_type, filtered_list = sort_and_filter(filtered_list, 1)
+vintage_color, filtered_list = sort_and_filter(filtered_list, 3)
+vintage_rarity, filtered_list = sort_and_filter(filtered_list, 5)
+vintage_name, filtered_list = sort_and_filter(filtered_list, 0)
+vintage_item = filtered_list[0]
+
+# Get data for Legacy
+legacy_list = []
+LEGACY_OWN = 0
+LEGACY_TOTAL = 0
+for card in raw_list:
+    if 'Legacy' in card[6]:
+        legacy_list.append(card)
+        LEGACY_TOTAL += card[6]['Legacy']
+        LEGACY_OWN += min(card[7], card[6]['Legacy'])
+LEGACY_CARDS = len(legacy_list)
+
+legacy_set, filtered_list = sort_and_filter(legacy_list, 4)
+legacy_type, filtered_list = sort_and_filter(filtered_list, 1)
+legacy_color, filtered_list = sort_and_filter(filtered_list, 3)
+legacy_rarity, filtered_list = sort_and_filter(filtered_list, 5)
+legacy_name, filtered_list = sort_and_filter(filtered_list, 0)
+legacy_item = filtered_list[0]
+
+# Get data for Commander
+commander_list = []
+COMMANDER_OWN = 0
+COMMANDER_TOTAL = 0
+for card in raw_list:
+    if 'Commander' in card[6]:
+        commander_list.append(card)
+        COMMANDER_TOTAL += card[6]['Commander']
+        COMMANDER_OWN += min(card[7], card[6]['Commander'])
+COMMANDER_CARDS = len(commander_list)
+
+commander_set, filtered_list = sort_and_filter(commander_list, 4)
+commander_type, filtered_list = sort_and_filter(filtered_list, 1)
+commander_color, filtered_list = sort_and_filter(filtered_list, 3)
+commander_rarity, filtered_list = sort_and_filter(filtered_list, 5)
+commander_name, filtered_list = sort_and_filter(filtered_list, 0)
+commander_item = filtered_list[0]
+
+# Get data for Pioneer
+pioneer_list = []
+PIONEER_OWN = 0
+PIONEER_TOTAL = 0
+for card in raw_list:
+    if 'Pioneer' in card[6]:
+        pioneer_list.append(card)
+        PIONEER_TOTAL += card[6]['Pioneer']
+        PIONEER_OWN += min(card[7], card[6]['Pioneer'])
+PIONEER_CARDS = len(pioneer_list)
+
+pioneer_set, filtered_list = sort_and_filter(pioneer_list, 4)
+pioneer_type, filtered_list = sort_and_filter(filtered_list, 1)
+pioneer_color, filtered_list = sort_and_filter(filtered_list, 3)
+pioneer_rarity, filtered_list = sort_and_filter(filtered_list, 5)
+pioneer_name, filtered_list = sort_and_filter(filtered_list, 0)
+pioneer_item = filtered_list[0]
+
+# Get data for Modern
+modern_list = []
+MODERN_OWN = 0
+MODERN_TOTAL = 0
+for card in raw_list:
+    if 'Modern' in card[6]:
+        modern_list.append(card)
+        MODERN_TOTAL += card[6]['Modern']
+        MODERN_OWN += min(card[7], card[6]['Modern'])
+MODERN_CARDS = len(modern_list)
+
+modern_set, filtered_list = sort_and_filter(modern_list, 4)
+modern_type, filtered_list = sort_and_filter(filtered_list, 1)
+modern_color, filtered_list = sort_and_filter(filtered_list, 3)
+modern_rarity, filtered_list = sort_and_filter(filtered_list, 5)
+modern_name, filtered_list = sort_and_filter(filtered_list, 0)
+modern_item = filtered_list[0]
+
+# Get data for Standard
+standard_list = []
+STANDARD_OWN = 0
+STANDARD_TOTAL = 0
+for card in raw_list:
+    if 'Standard' in card[6]:
+        standard_list.append(card)
+        STANDARD_TOTAL += card[6]['Standard']
+        STANDARD_OWN += min(card[7], card[6]['Standard'])
+STANDARD_CARDS = len(standard_list)
+
+standard_set, filtered_list = sort_and_filter(standard_list, 4)
+standard_type, filtered_list = sort_and_filter(filtered_list, 1)
+standard_color, filtered_list = sort_and_filter(filtered_list, 3)
+standard_rarity, filtered_list = sort_and_filter(filtered_list, 5)
+standard_name, filtered_list = sort_and_filter(filtered_list, 0)
+standard_item = filtered_list[0]
+
+# Get data for Pauper
+pauper_list = []
+PAUPER_OWN = 0
+PAUPER_TOTAL = 0
+for card in raw_list:
+    if 'Pauper' in card[6]:
+        pauper_list.append(card)
+        PAUPER_TOTAL += card[6]['Pauper']
+        PAUPER_OWN += min(card[7], card[6]['Pauper'])
+PAUPER_CARDS = len(pauper_list)
+
+pauper_set, filtered_list = sort_and_filter(pauper_list, 4)
+pauper_type, filtered_list = sort_and_filter(filtered_list, 1)
+pauper_color, filtered_list = sort_and_filter(filtered_list, 3)
+pauper_rarity, filtered_list = sort_and_filter(filtered_list, 5)
+pauper_name, filtered_list = sort_and_filter(filtered_list, 0)
+pauper_item = filtered_list[0]
+
+if __name__ == "__main__":
+    if os.getcwd().endswith('card-minis-boardgames'):
+        out_file_h = open("card_games/output/MTGOut.txt", 'w', encoding="UTF-8")
+    else:
+        out_file_h = open("output/MTGOut.txt", 'w', encoding="UTF-8")
+
+    double_print(f"Tracking {len(raw_list)} cards.", out_file_h)
+    SUMMARY_STRING = f"Have {TOTAL_OWN} out of {TOTAL_MAX} total cards for a playset " \
+        f"- {100* TOTAL_OWN/TOTAL_MAX:.2f} percent"
+    double_print(SUMMARY_STRING, out_file_h)
+
+    double_print("\n*** VINTAGE ***", out_file_h)
+    double_print(f"There are {VINTAGE_CARDS} Vintage legal cards", out_file_h)
+    VINT_STR = f"Have {VINTAGE_OWN} out of {VINTAGE_TOTAL} - " \
+        f"{100* VINTAGE_OWN/VINTAGE_TOTAL:.2f} percent of a playset"
+    double_print(VINT_STR, out_file_h)
+
+    PURCH_STR = f"Chosen card is a(n) {vintage_type} from {vintage_set} - {vintage_name}. " + \
+        f"I own {vintage_item[7]} of {vintage_item[6]['Vintage']}"
+    double_print(PURCH_STR, out_file_h)
+
+    double_print("\n*** LEGACY ***", out_file_h)
+    double_print(f"There are {LEGACY_CARDS} Legacy legal cards", out_file_h)
+    LEGACY_STR = f"Have {LEGACY_OWN} out of {LEGACY_TOTAL} - " \
+        f"{100* LEGACY_OWN/LEGACY_TOTAL:.2f} percent of a playset"
+    double_print(LEGACY_STR, out_file_h)
+
+    PURCH_STR = f"Chosen card is a(n) {legacy_type} from {legacy_set} - {legacy_name}. " + \
+        f"I own {legacy_item[7]} of {legacy_item[6]['Legacy']}"
+    double_print(PURCH_STR, out_file_h)
+
+    double_print("\n*** COMMANDER ***", out_file_h)
+    double_print(f"There are {COMMANDER_CARDS} Commander legal cards", out_file_h)
+    COMMANDER_STR = f"Have {COMMANDER_OWN} out of {COMMANDER_TOTAL} - " \
+        f"{100* COMMANDER_OWN/COMMANDER_TOTAL:.2f} percent of a playset"
+    double_print(COMMANDER_STR, out_file_h)
+
+    PURCH_STR = f"Chosen card is a(n) {commander_type} from {commander_set} - {commander_name}" + \
+        f". I own {commander_item[7]} of {commander_item[6]['Commander']}"
+    double_print(PURCH_STR, out_file_h)
+
+    double_print("\n*** PIONEER ***", out_file_h)
+    double_print(f"There are {PIONEER_CARDS} Pioneer legal cards", out_file_h)
+    PIONEER_STR = f"Have {PIONEER_OWN} out of {PIONEER_TOTAL} - " \
+        f"{100* PIONEER_OWN/PIONEER_TOTAL:.2f} percent of a playset"
+    double_print(PIONEER_STR, out_file_h)
+
+    PURCH_STR = f"Chosen card is a(n) {pioneer_type} from {pioneer_set} - {pioneer_name}" + \
+        f". I own {pioneer_item[7]} of {pioneer_item[6]['Pioneer']}"
+    double_print(PURCH_STR, out_file_h)
+
+    double_print("\n*** MODERN ***", out_file_h)
+    double_print(f"There are {MODERN_CARDS} Modern legal cards", out_file_h)
+    MODERN_STR = f"Have {MODERN_OWN} out of {MODERN_TOTAL} - " \
+        f"{100* MODERN_OWN/MODERN_TOTAL:.2f} percent of a playset"
+    double_print(MODERN_STR, out_file_h)
+
+    PURCH_STR = f"Chosen card is a(n) {modern_type} from {modern_set} - {modern_name}" + \
+        f". I own {modern_item[7]} of {modern_item[6]['Modern']}"
+    double_print(PURCH_STR, out_file_h)
+
+    double_print("\n*** STANDARD ***", out_file_h)
+    double_print(f"There are {STANDARD_CARDS} Standard legal cards", out_file_h)
+    STANDARD_STR = f"Have {STANDARD_OWN} out of {STANDARD_TOTAL} - " \
+        f"{100* STANDARD_OWN/STANDARD_TOTAL:.2f} percent of a playset"
+    double_print(STANDARD_STR, out_file_h)
+
+    PURCH_STR = f"Chosen card is a(n) {standard_type} from {standard_set} - {standard_name}" + \
+        f". I own {standard_item[7]} of {standard_item[6]['Standard']}"
+    double_print(PURCH_STR, out_file_h)
+
+    double_print("\n*** PAUPER ***", out_file_h)
+    double_print(f"There are {PAUPER_CARDS} Pauper legal cards", out_file_h)
+    PAUPER_STR = f"Have {PAUPER_OWN} out of {PAUPER_TOTAL} - " \
+        f"{100* PAUPER_OWN/PAUPER_TOTAL:.2f} percent of a playset"
+    double_print(PAUPER_STR, out_file_h)
+
+    PURCH_STR = f"Chosen card is a(n) {pauper_type} from {pauper_set} - {pauper_name}" + \
+        f". I own {pauper_item[7]} of {pauper_item[6]['Pauper']}"
+    double_print(PURCH_STR, out_file_h)
