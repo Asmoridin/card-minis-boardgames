b82e1b5c4ad33100381877cd3c5c40a913baf9b5
Steve P <steven.pow@gmail.com>
2024-09-17 10:51:55 -0400

    Weatherlight Done
    - All cards for Weatherlight are now in DB
    - Cleaned up Mirage Block decks.

--- card_games/magic_gathering.py
+++ card_games/magic_gathering.py
@@ -350,9 +350,9 @@ restrictions = parse_restrictions(restr_file_h.readlines())
 restr_file_h.close()
 
 SET_CHECK = 0
-CHECK_SET = "Weatherlight"
-CHECK_AMOUNT = 167
-SET_CHECK += 10 # Extra basic lands
+CHECK_SET = "Tempest"
+CHECK_AMOUNT = 350
+SET_CHECK += 15 # Extra basic lands
 
 TOTAL_OWN = 0
 TOTAL_MAX = 0
