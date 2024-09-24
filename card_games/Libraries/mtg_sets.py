2389e817a7573e19e9b8f2a9b5c6aace1875b664
Steve P <steven.pow@gmail.com>
2024-09-24 14:20:12 -0400

    Changing decks to handle new EDH Banlist

--- card_games/Libraries/mtg_sets.py
+++ card_games/Libraries/mtg_sets.py
@@ -53,6 +53,7 @@ LEGACY_SETS = ["Limited Edition Alpha", "Limited Edition Beta", "Unlimited Editi
     "Weatherlight", "Fallen Empires", "Promo set for Gatherer", "Urza's Legacy", "Urza's Destiny",
     "From the Vault: Transform", "Ravnica Allegiance Mythic Edition", "Stronghold",
     "Duskmourn: House of Horror Commander", "Signature Spellbook: Gideon", "From the Vault: Lore",
+    "Ugin's Fate promos",
     ]
 
 NON_SETS = ['Unglued', "Unhinged", "Unstable", "Unsanctioned", 'Vanguard']
