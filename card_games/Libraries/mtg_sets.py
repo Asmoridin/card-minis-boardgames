d1f6571d825dc96e3c787ffba4a6c71b90de83d0
Steve P <steven.pow@gmail.com>
2024-10-03 10:04:10 -0400

    Finished data for Portal Second Age

--- card_games/Libraries/mtg_sets.py
+++ card_games/Libraries/mtg_sets.py
@@ -51,7 +51,7 @@ LEGACY_SETS = ["Limited Edition Alpha", "Limited Edition Beta", "Unlimited Editi
     "Zendikar Expeditions", "Zendikar Rising Expeditions", "Antiquities", "Commander's Arsenal",
     "Outlaws of Thunder Junction Breaking News", "March of the Machine Multiverse Legends",
     "Weatherlight", "Fallen Empires", "Promo set for Gatherer", "Urza's Legacy", "Urza's Destiny",
-    "From the Vault: Transform", "Ravnica Allegiance Mythic Edition", "Stronghold",
+    "From the Vault: Transform", "Ravnica Allegiance Mythic Edition", "Stronghold", "Scourge",
     "Duskmourn: House of Horror Commander", "Signature Spellbook: Gideon", "From the Vault: Lore",
     "Ugin's Fate promos",
     ]
