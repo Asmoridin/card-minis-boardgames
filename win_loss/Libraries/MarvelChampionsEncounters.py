77b8db1b7356c5d534a551d56b568ec132b5dbe6
Steve P <steven.pow@gmail.com>
2024-11-18 14:44:59 -0500

    Added SFB WL, Various updates
    - Closes #28
    - Added two new Targ NCUs
    - DBS and OP cards added to inventory
    - Magneto pack added to Marvel Champions

--- win_loss/Libraries/MarvelChampionsEncounters.py
+++ win_loss/Libraries/MarvelChampionsEncounters.py
@@ -18,7 +18,7 @@ modular_encounters = ['Bomb Scare', 'Masters of Evil', 'Under Attack', 'Legions 
     'Acolytes', 'Military Grade', 'Mutant Slayers', 'Nasty Boys', 'Black Tom Cassidy', 'Flight',
     'Super Strength', 'Telepathy', 'Extreme Measures', 'Mutant Insurrection', 'Infinites',
     'Dystopian Nightmare', 'Hounds', 'Dark Riders', 'Blue Moon', 'Genosha', 'Savage Land',
-    'Celestial Tech', 'Clan Akkaba', 'Sauron', 'Arcade', 'Crazy Gang',
+    'Celestial Tech', 'Clan Akkaba', 'Sauron', 'Arcade', 'Crazy Gang', 'Hellfire'
 ]
 mojo_encounters = ['Crime', 'Fantasy', 'Horror', 'Sci-Fi', 'Sitcom', 'Western']
 modular_encounters = modular_encounters + mojo_encounters
